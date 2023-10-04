af4404465fb683dcc9aecbc10d3e835fb2617a22
DESKTOP-TREDQN8\Andrey <andrprg@yandex.ru>
2023-10-04 14:55:40 +0300

    Добавлены интерсепторы

---
+++ src/app/data/interceptors/api-prefix.interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { HOST_URL } from 'src/environments/environment';
+
+
+@Injectable()
+export class ApiPrefixInterceptor implements HttpInterceptor {
+  
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!/^(http|https):/i.test(request.url)) {
+      request = request.clone({ url: HOST_URL + request.url });
+    }
+    return next.handle(request);
+  }
+}
