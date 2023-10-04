975efe883edb3ea45448dd0d7bd6001f94a12ce7
DESKTOP-TREDQN8\Andrey <andrprg@yandex.ru>
2023-10-04 14:17:04 +0300

    Добавлены стили и настроен layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './ui/layout/components/header/header.component';
+import { MainComponent } from './ui/layout/components/main/main.component';
+import { FooterComponent } from './ui/layout/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
